2d2e8f07a690e3fb860fd6e34253f5b6d97d4eae
test_ADO <ravikumar.katrenikona@wisseninfotech.com>
2021-09-23 12:34:08 +0530

    added project

---
+++ DB project/demowissensqlpoc/dbo/Tables/Wissenpoc.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Wissenpoc] (
+    [empno]   INT          NOT NULL,
+    [empname] VARCHAR (15) NULL,
+    [Job]     VARCHAR (10) NULL,
+    [Sal]     REAL         NULL,
+    PRIMARY KEY CLUSTERED ([empno] ASC)
+);
+
